844e25be717feb89979f35c12f27ed12632a62c6
SviataslauKrp <kirpik.svtatoslav@yandex.ru>
2022-09-27 18:40:36 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,193 @@ public class Main {
     public static void main(String[] args) {
 
         final String delimiter = "================================\n";
+        final String hat = "Управляющие конструкции";
+        final String hat2 = "Циклы";
+        final String hat3 = "Дополнительное задание";
 
+        //управляющие конструкции
+
+        System.out.println(hat +  "1:");
         dz1();
         System.out.println(delimiter);
-        
+
+        System.out.println(hat +  "2:");
         dz2();
         System.out.println(delimiter);
 
+        System.out.println(hat +  "3:");
         dz3();
         System.out.println(delimiter);
 
+        System.out.println(hat +  "4:");
         dz4();
+        System.out.println(delimiter);
+
+        //циклы
+
+        System.out.println(hat2 + "1:");
+        dz5();
+        System.out.println(delimiter);
+
+        System.out.println(hat2 + "2:");
+        dz6();
+        System.out.println(delimiter);
+
+        System.out.println(hat2 + "3:");
+        dz7();
+        System.out.println(delimiter);
+
+        System.out.println(hat2 + "4:");
+        dz8();
+        System.out.println(delimiter);
+
+        System.out.println(hat2 + "5:");
+        dz9();
+        System.out.println(delimiter);
+
+        System.out.println(hat2 + "6:");
+        dz10();
+        System.out.println(delimiter);
+
+        //Дополнительные задания
+
+        System.out.println(hat3 + "1:");
+        dz11();
+        System.out.println(delimiter);
+
+        System.out.println(hat3 + "2:");
+        dz12();
+        System.out.println(delimiter);
+        System.err.println("Вопрос о закрытии Console\n" +
+                "Вырвавнивание массивов");
+        
     }
 
-//    4. По введенному номеру определить цвет радуги (1 – красный, 4 – зеленый и т. д.).
+    //      12. Напишите программу, которая выводит на консоль таблицу умножения
+    private static void dz12() {
+
+        int[][] table = new int[10][10];
+
+        for (int i = 0; i < table.length; i++) {
+            table[0][i] = i;
+            table[i][0] = i;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                table[j][i] = table[0][i] * table[j][0];
+            }
+        }
+        int colorNumber = 29;
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[0].length; j++) {
+               // System.out.print(table[j][i] + " ");
+
+                System.out.printf((char) 27 + "[%dm " + table[j][i], colorNumber);
+            }
+            colorNumber++;
+            System.out.println();
+
+        }
+
+    }
+
+    //      11. Выведите на экран первые 11 членов последовательности Фибоначчи.
+    private static void dz11() {
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        System.out.print(firstNumber + " " + secondNumber + " ");
+
+// 9 итераций потому, как первые 2 числа ряда выведены выше
+        for (int i= 0; i < 9; i++) {
+
+            int nextNumber = firstNumber + secondNumber;
+
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+
+            System.out.print(nextNumber + " ");
+        }
+        System.out.println();
+    }
+
+    //      10. Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно
+    private static void dz10() {
+
+        for (int i = 10; i < 21; i++) {
+            int square = (int) Math.pow(i, 2);
+            System.out.print(square + " ");
+        }
+        System.out.println();
+    }
+
+    //      9. Вывести 10 первых чисел последовательности 0, -5,-10,-15..
+    private static void dz9() {
+
+        for (int i = 0; i > -50; i-=5) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    //      8. Необходимо, чтоб программа выводила на экран вот такую
+    //последовательность: 7 14 21 28 35 42 49 56 63 70 77 84 91 98.
+    //В решении используйте цикл while.
+    private static void dz8() {
+
+        final int multipliable = 7;
+        int multiplier = 1;
+        int multiplication = 0;
+
+        while (multiplication != 98) {
+            multiplication = multipliable * multiplier;
+            System.out.print(multiplication + " ");
+            multiplier++;
+        }
+        System.out.println();
+    }
+
+    //      7. Напишите программу, где пользователь вводит любое целое положительное число. А программа суммирует все числа от 1 до
+    //введенного пользователем числа. Для ввода числа воспользуйтесь классом Scanner
+    private static void dz7() {
+
+        Scanner console = new Scanner(System.in);
+        int number = console.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i < number; i++) {
+            sum +=i;
+        }
+        System.out.println(sum);
+
+        //Scanner не закрыт: https://stackoverflow.com/questions/13042008/java-util-nosuchelementexception-scanner-reading-user-input
+    }
+
+    //      6. Необходимо вывести на экран числа от 5 до 1. При решении используйте операцию декремента (--).
+    private static void dz6() {
+
+        for (int i = 5; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    //     5. При помощи цикла for вывести на экран нечетные числа от 1 до 99. При решении используйте операцию инкремента (++).
+    private static void dz5() {
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+
+    }
+
+    //    4. По введенному номеру определить цвет радуги (1 – красный, 4 – зеленый и т. д.).
     private static void dz4() {
 
         Scanner scanner = new Scanner(System.in);
@@ -50,7 +223,7 @@ public class Main {
     }
 
     //    3. Для введенного числа t (температура на улице) вывести. Если t>–5, то вывести «Тепло». Если –5>= t > –20, то вывести «Нормально».
-//    Если –20>= t, то вывести «Холодно»
+    //    Если –20>= t, то вывести «Холодно»
     private static void dz3() {
 
         Scanner console = new Scanner(System.in);
@@ -69,7 +242,7 @@ public class Main {
 
     }
 
-//  2. Написать программу для вывода названия поры года по номеру месяца. При решении используйте оператор if-else-if.
+    //  2. Написать программу для вывода названия поры года по номеру месяца. При решении используйте оператор if-else-if.
     private static void dz2() {
 
         final int startIndex = 1;
@@ -91,7 +264,7 @@ public class Main {
 
     }
 
-//    1. Написать программу для вывода названия поры года по номеру месяца. При решении используйте оператор switch-case
+    //    1. Написать программу для вывода названия поры года по номеру месяца. При решении используйте оператор switch-case
     private static void dz1() {
 
         final int startIndex = 1;
